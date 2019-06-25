6e56dfa101613081b8a273dfe78ccd1513eeaac7
Deep Mehta <deepmehta.it@live.com>
2019-06-25 15:16:16 -0700

    Creating RecyclerView, Adaptor, ViewHolder and retriving data from DB.

--- Android/app/src/main/java/com/deepmehtait/priorityapp/MainActivity.java
+++ Android/app/src/main/java/com/deepmehtait/priorityapp/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.deepmehtait.priorityapp.adaptor.NoteAdaptor;
 import com.deepmehtait.priorityapp.room.entity.Note;
 import com.deepmehtait.priorityapp.viewmodel.NoteViewModel;
 
@@ -22,12 +25,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view_display);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdaptor noteAdaptor = new NoteAdaptor();
+        recyclerView.setAdapter(noteAdaptor);
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
                 // Update Recycler View here.
-                Toast.makeText(MainActivity.this, "on Changed", Toast.LENGTH_SHORT).show();
+                noteAdaptor.setNotes(notes);
             }
         });
     }
---
+++ Android/app/src/main/java/com/deepmehtait/priorityapp/adaptor/NoteAdaptor.java
@@ -0,0 +1,61 @@
+package com.deepmehtait.priorityapp.adaptor;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.deepmehtait.priorityapp.R;
+import com.deepmehtait.priorityapp.room.entity.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdaptor extends RecyclerView.Adapter<NoteAdaptor.NoteHolder> {
+
+    private List<Note> notes = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_item, parent, false);
+
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        Note currentNote = notes.get(position);
+        holder.tvTitle.setText(currentNote.getTitle());
+        holder.tvPriority.setText(String.valueOf(currentNote.getPriority()));
+        holder.tvDescription.setText(currentNote.getDescription());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+        private TextView tvTitle;
+        private TextView tvPriority;
+        private TextView tvDescription;
+
+        public NoteHolder(View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.text_view_title);
+            tvPriority = itemView.findViewById(R.id.text_view_priority);
+            tvDescription = itemView.findViewById(R.id.text_view_description);
+        }
+    }
+}
