8e746bbcd99fff7d2d0c2d8f7bd2c15466589cfe
Deep Mehta <deepmehta.it@live.com>
2019-06-25 22:45:33 -0700

    Adding new note and saving it.

---
+++ Android/app/src/main/java/com/deepmehtait/priorityapp/AddNoteActivity.java
@@ -0,0 +1,81 @@
+package com.deepmehtait.priorityapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    public static final String EXTRA_TITLE = "com.deepmehtait.priorityapp.EXTRA_TITLE";
+    public static final String EXTRA_DESC = "com.deepmehtait.priorityapp.EXTRA_DESC";
+    public static final String EXTRA_PRIORITY = "com.deepmehtait.priorityapp.EXTRA_PRIORITY";
+
+    private EditText etTitle;
+    private EditText etDescription;
+    private NumberPicker npPriority;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+
+        etTitle = findViewById(R.id.edit_text_title);
+        etDescription = findViewById(R.id.edit_text_description);
+        npPriority = findViewById(R.id.number_picker_priority);
+
+        npPriority.setMaxValue(10);
+        npPriority.setMinValue(1);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add Note");
+
+    }
+
+    private void saveNote() {
+        String title = etTitle.getText().toString();
+        String desc = etDescription.getText().toString();
+        int priority = npPriority.getValue();
+
+        if (title.trim().isEmpty() || desc.trim().isEmpty()) {
+            Toast.makeText(this, "Please enter Title & description", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        Intent data = new Intent();
+        data.putExtra(EXTRA_TITLE, title);
+        data.putExtra(EXTRA_DESC, desc);
+        data.putExtra(EXTRA_PRIORITY, priority);
+
+        setResult(RESULT_OK, data);
+        finish();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_note:
+                saveNote();
+                return true;
+             default:
+                 return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
--- Android/app/src/main/java/com/deepmehtait/priorityapp/MainActivity.java
+++ Android/app/src/main/java/com/deepmehtait/priorityapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.deepmehtait.priorityapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -7,17 +8,21 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.deepmehtait.priorityapp.adaptor.NoteAdaptor;
 import com.deepmehtait.priorityapp.room.entity.Note;
 import com.deepmehtait.priorityapp.viewmodel.NoteViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int ADD_NOTE_REQUEST = 1;
     private NoteViewModel noteViewModel;
 
     @Override
@@ -25,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FloatingActionButton floatingActionButton = findViewById(R.id.button_add_note);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+                startActivityForResult(intent, ADD_NOTE_REQUEST);
+
+            }
+        });
         RecyclerView recyclerView = findViewById(R.id.recycler_view_display);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
@@ -39,5 +53,24 @@ public class MainActivity extends AppCompatActivity {
                 noteAdaptor.setNotes(notes);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ADD_NOTE_REQUEST && resultCode == RESULT_OK) {
+            String title = data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String desc = data.getStringExtra(AddNoteActivity.EXTRA_DESC);
+            int priority = data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY, 1);
+
+            Note note = new Note(title, desc, priority);
+            noteViewModel.insert(note);
+
+            Toast.makeText(this, "Note Added", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(this, "Note not saved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
